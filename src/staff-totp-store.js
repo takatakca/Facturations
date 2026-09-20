1dd46a744f8fe5d86e35d8ebcd3f0ffbc462331d
takatakca <takatak.ca@gmail.com>
2026-09-20 11:59:52 -0700

    Keep original UUID validation unchanged in MFA pool fix

--- src/staff-totp-store.js
+++ src/staff-totp-store.js
@@ -2,7 +2,7 @@
 
 const crypto = require('node:crypto');
 
-const UUID = /^[a-f0-9]{8}-[a-f0-9]{4}-[1-8][a-f0-9]{3}-[89ab][a-f0-9]{3}-[89ab][a-f0-9]{12}$/i;
+const UUID = /^[a-f0-9]{8}-[a-f0-9]{4}-[1-8][a-f0-9]{3}-[89ab][a-f0-9]{3}-[a-f0-9]{12}$/i;
 const CODE = /^[0-9]{6}$/;
 const ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567';
 
